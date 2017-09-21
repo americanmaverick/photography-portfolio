4520914419070df3537c962e8e212fad5df47c26
Benjamin Bergstein <bennyjbergstein@gmail.com>
2017-09-21 09:27:52 -0700

    Refactor JS a little, remove silly CSS gradient background.

--- site/javascript/application.js
+++ site/javascript/application.js
@@ -1,33 +1,41 @@
 function main() {
-  window.addEventListener('click', function(e) {
-    handleCloseup(e);
-  });
-  
-  backToGallery.addEventListener('click', function() {
-    gallery.classList.toggle('hidden', false);
-    closeup.classList.toggle('hidden', true);
-  });
+  window.addEventListener('click', displayCloseup);
+  backToGallery.addEventListener('click', hideCloseup);
 }
 
-main.closeup = undefined;
+main.currentCloseup = undefined;
 
-function handleCloseup(e) {
-  var photo, preloader;
+function displayCloseup(e) {
+  var photo;
   
-  photo = e.target.dataset.photo;
-  main.closeup = photo;
+  main.currentCloseup = photo = e.target.dataset.photo;
   
   if (photo) {
-    closeupImage.src = './photos/med/'+photo;
-    preloader = new Image();
-    preloader.onload = function() {
-      if (main.closeup === photo) closeupImage.src = this.src;
-    };
-    preloader.src = './photos/lg/'+photo;
-    closeupLink.href = './photos/orig/'+photo;
+    closeupImage.src = size('med', photo);
+    
+    preload(photo);
+    
+    closeupLink.href = size('orig', photo);
     gallery.classList.toggle('hidden', true);
     closeup.classList.toggle('hidden', false);
   }
 }
 
+function hideCloseup() {
+  gallery.classList.toggle('hidden', false);
+  closeup.classList.toggle('hidden', true);
+}
+
+function preload(photo) {
+  var preloader;
+  preloader = new Image();
+  preloader.onload = function() {
+    if (main.currentCloseup === photo) closeupImage.src = this.src;
+  };
+  preloader.src = size('lg', photo);
+}
+
+function size(sizeName, photo) {
+  return './photos/'+sizeName+'/'+photo;
+}
 main();
