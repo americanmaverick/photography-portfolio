ad9c2f011caecac11d32a6c66348bc649e4aec78
Benjamin Bergstein <bennyjbergstein@gmail.com>
2018-08-19 20:47:37 -0700

    Updates

--- docs/javascript/application.js
+++ docs/javascript/application.js
@@ -1,6 +1,7 @@
 function main() {
   on('hashchange', window, handlePageState);
   handlePageState();
+  on('keyup', window, cyclePhoto)
 }
 
 main.currentCloseup = undefined;
@@ -8,7 +9,7 @@ main.currentCloseup = undefined;
 function handlePageState() {
   var photo;
   photo = window.location.hash.substr(1);
-  
+
   if (photo === '') {
     hideCloseup()
   } else {
@@ -16,14 +17,53 @@ function handlePageState() {
   }
 }
 
+function nextPhoto () {
+  var images = document.querySelectorAll('.image-tile'),
+      re = new RegExp(main.currentCloseup),
+      cycleImageIsCurrent = false,
+      nextImage;
+
+  images.forEach(function(tile) {
+    var imageSrc = tile.children[0].src;
+
+    if (cycleImageIsCurrent) {
+      nextImage = tile;
+    }
+
+    if (re.test(imageSrc)) {
+      cycleImageIsCurrent = true;
+    } else {
+      cycleImageIsCurrent = false;
+    }
+  });
+
+  if (!nextImage) {
+    nextImage = images[0];
+  }
+
+  window.location = nextImage.href;
+}
+
+function cyclePhoto(e) {
+  if (!main.currentCloseup) return;
+  var keyCode = e.keyCode;
+
+  if (keyCode == 39) nextPhoto();
+  else if (keyCode == 27) window.location = '#';
+  else return;
+
+  e.stopPropagation()
+  e.preventDefault()
+}
+
 function displayCloseup(photo) {
   main.currentCloseup = photo;
-  
+
   if (photo) {
     closeupImage.src = size('med', photo);
-    
+
     preload(photo);
-    
+
     closeupLink.href = size('orig', photo);
     gallery.classList.toggle('hidden', true);
     closeup.classList.toggle('hidden', false);
--- stubborn_helper.rb
+++ stubborn_helper.rb
@@ -1,6 +1,12 @@
+require 'securerandom'
+
 def photos
-  Dir.glob('./site/photos/orig/*.JPG').map do |path|
+  Dir.glob('./docs/photos/orig/*.JPG').map do |path|
     Pathname(path).basename
   end
+end
+
+def cache_buster
+  "#{Time.new.strftime('%F')}-#{SecureRandom.hex(3)}"
 end
 
